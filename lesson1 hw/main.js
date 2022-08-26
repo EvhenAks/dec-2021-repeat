74cae96dd07143ddf8910e13211afce768e7f4ed
EvhenAks <aksjonovev@gmail.com>
2022-08-26 22:28:53 +0300

    js hw1

---
+++ lesson1 hw/main.js
@@ -0,0 +1,74 @@
+// let hello = 'hello'
+// let owu = 'owu'
+// let com = 'com'
+// console.log(hello,owu,com)
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName = 'Evhen'
+// let middleName = 'Vitaliyovuch'
+// let lastName = 'Aksjonov'
+// let person = firstName + ' ' + middleName + ' ' + lastName
+// console.log(person)
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+// // ******yes
+// let pib = prompt('name') + prompt('batko') + prompt('year');
+// console.log(pib);
+// // - Створити масив об'єктів з назвою books з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let books = [{
+//     name: 'harry',
+//     pages: 120,
+//     genre: 'fantasy',
+//     aughtor: 'Joan'
+// },
+//     {
+//         name: 'vasilisk',
+//         pages: 403,
+//         genre: 'polution',
+//         aughtor: 'Funk'
+//     }];
+// console.log(books[0]);
+// console.log(books[1]);
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+
+// let book = {
+//     title: 'book',
+//     pages: 4000,
+//     genre: 'rpg'
+// }
+// console.log(book);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let user = [
+//     {
+//     name: 'oleg',
+//     username: 'oleg228',
+//     password: 'olegnagibator220'
+// },  {
+//     name: 'gregre',
+//     username: 'oleg228',
+//     password: 'olegnagibator221'
+// },  {
+//     name: 'sdgasdgas',
+//     username: 'oleg228',
+//     password: 'olegnagibator222'
+// },  {
+//     name: 'adasdasd',
+//     username: 'oleg228',
+//     password: 'olegnagibator223'
+// },
+// ];
+// console.log(user[0].password)
+// console.log(user[1].password)
+// console.log(user[2].password)
+// console.log(user[3].password)
