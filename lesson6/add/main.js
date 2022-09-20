418672a6a81d3c3607838de5bac92b85542cdc69
EvhenAks <aksjonovev@gmail.com>
2022-09-21 00:58:24 +0300

    add hw5, hw6

---
+++ lesson6/add/main.js
@@ -0,0 +1,15 @@
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+
+let str = 'наслаждение';
+
+let cutString = function (str,n){
+
+    for (let i = 0; i > str.length; i++){
+        if (str[i] % n === 0){
+           return str.concat(',')
+        }
+    }
+}
+
+console.log(cutString(str, 2));
